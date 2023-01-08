f01b4a45f8c4a66cc8f94f4acf2d83929dbbc670
tpremoli <tomas@premoli.org>
2023-01-08 11:47:23 -0800

    Created first training files

---
+++ training/constants.py
@@ -0,0 +1,9 @@
+# Supported architectures
+VGG_16 = "VGG16"
+VGG_19 = "VGG19"
+
+RES_NET_50 = "ResNet50"
+RES_NET_157 = "ResNet157"
+
+DENSE_NET_121 = "DenseNet121"
+DENSE_NET_201 = "DenseNet201"
---
+++ training/model.py
@@ -0,0 +1,9 @@
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.applications.vgg16 import preprocess_input, decode_predictions
+from tensorflow.keras.preprocessing import image
+import numpy as np
+
+class Model:
+    def __init__(self, application):
+        pass
+    
