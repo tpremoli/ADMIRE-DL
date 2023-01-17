d92a6aee3075a4bd4b979311ce7fcb059154c14e
tpremoli <tomas@premoli.org>
2023-01-17 00:53:17 +0000

    Created conda env

--- cli.py
+++ cli.py
@@ -1,7 +1,6 @@
 import argparse
 from preprocessing.run_scripts import prep_adni, prep_kaggle
 
-# Test commit forWSL
 def main():
     parser = argparse.ArgumentParser(description='This is the CLI tool to use the {APP_NAME} tool')
 
