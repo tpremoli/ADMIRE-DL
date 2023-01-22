959dbd8e76c8a358916e08a0a29b940c698b33be
tpremoli <tomas@premoli.org>
2023-01-22 18:57:35 +0000

    Created prep_raw.py to switch data prep to functional apprach

---
+++ src/preprocessing/prep_raw.py
@@ -0,0 +1,83 @@
+import shutil
+import pickle
+import numpy as np
+import nibabel as nib
+import fsl.wrappers.fsl_anat as fsl_anat
+import fsl.wrappers.fslmaths as fsl_maths
+from PIL import Image
+from pathlib import Path
+from datetime import datetime
+
+cwd = Path().resolve()
+filedir = Path(__file__).parent.resolve()
+
+
+def prep_raw_mri(scan_loc, scan_name, out_dir, group, sex):
+    if group is None or sex is None:
+        raise ValueError(
+            "ERROR: Scan instatiation requires group, sex, and age values!")
+
+    start_time = datetime.now()
+
+    scan_location = Path(cwd, scan_loc).resolve()
+
+    print("Launching FSL scripts for scan {}".format(scan_name))
+    nii_path = run_fsl(scan_location, scan_name, out_dir)
+
+    print("FSL scripts complete. Processed MRI found in {}".format(nii_path))
+
+    # To access slices:
+    # sagittal = self.data[26, :, :] <- 26th slice along sagittal
+    # coronal = self.data[:, 30, :] <- 30th slice along coronal
+    # axial = self.data[:, :, 50] <- 50th slice along axial
+
+    end_time = datetime.now()
+
+    return end_time-start_time
+
+
+def run_fsl(scan_location, scan_name, out_dir):
+    niifile = ""
+    # Finding nii file. This just does it for the first file found in subfolders, but this should run for multiple samples in the future
+    for p in scan_location.rglob("*"):
+        if p.name.endswith(".nii"):
+            niifile = p
+            break
+
+    # The tmp_dir directory will be used to store all the fsl_anat info
+    tmp_dir = Path(
+        filedir, "../../out/preprocessed_datasets/tmp", scan_name).resolve()
+
+    # Running fsl_anat (we don't need tissue segmentation nor subcortical segmentation)
+    fsl_anat(img=niifile, out=tmp_dir, noseg=True, nosubcortseg=True)
+
+    # fsl_anat adds .anat to end of output directory
+    anat_dir = Path("{}.anat".format(tmp_dir))
+
+    # This is the outputted nonlinear transformed brain
+    mni_nonlin = Path(anat_dir, "T1_to_MNI_nonlin.nii.gz")
+
+    # This is the outputted brain mask
+    brain_mask = Path(anat_dir, "MNI152_T1_2mm_brain_mask_dil1.nii.gz")
+
+    final_brain = Path(
+        out_dir, "MRIs/{}_processed.nii.gz".format(scan_name))
+
+    # We multiply the MNI registered brain by the brain mask to have a final preprocessed brain
+    fsl_maths(mni_nonlin).mul(brain_mask).run(final_brain)
+
+    # Copy log from anat dir to out dir
+    # logfile = Path(anat_dir, "log.txt")
+    # final_logfile = Path(out_dir, "{}.log".format(self.scan_name))
+    # shutil.copyfile(logfile, final_logfile)
+
+    # clearing all the .anat files (unnecessary now)
+    shutil.rmtree(anat_dir)
+
+    return final_brain
+
+
+def get_data_from_nii(self):
+    imgfile = nib.load(self.nii_path)
+    return np.array(imgfile.dataobj)
+
