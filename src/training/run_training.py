a861b3e0347950acc04d95248da14dbcac728d62
tpremoli <tomas@premoli.org>
2023-01-24 00:21:01 +0000

    added lr reducer on plateau

--- src/training/run_training.py
+++ src/training/run_training.py
@@ -1,6 +1,8 @@
 import yaml
 import shutil
+import numpy as np
 from tensorflow.config import list_logical_devices
+from tensorflow.keras.callbacks import ReduceLROnPlateau
 from datetime import datetime
 from pathlib import Path
 from ..constants import *
@@ -9,22 +11,24 @@ from .utils import gen_subsets, create_model, plot_data
 cwd = Path().resolve()
 filedir = Path(__file__).parent.resolve()
 
+
 def load_training_task(file_loc):
     with open(Path(cwd, file_loc).resolve(), "r") as f:
         yamlfile = yaml.safe_load(f)
-        
+
         keys = yamlfile.keys()
         if "task_name" not in keys:
             raise ValueError("Task config requires a task_name attribute!")
         if "dataset" not in keys:
             raise ValueError("Task config requires a dataset attribute!")
 
-        for path in Path(filedir, "../../out/trained_models").glob(yamlfile["task_name"]):        
-            raise ValueError("Task with name {} already exists in {}!".format(yamlfile["task_name"], path))
+        for path in Path(filedir, "../../out/trained_models").glob(yamlfile["task_name"]):
+            raise ValueError("Task with name {} already exists in {}!".format(
+                yamlfile["task_name"], path))
 
         options = yamlfile["options"]
         optionkeys = options.keys()
-        
+
         if "architecture" not in optionkeys:
             raise ValueError("Task config requires an architecture attribute!")
         if "approach" not in optionkeys:
@@ -37,21 +41,24 @@ def load_training_task(file_loc):
         # TODO: change access to .get("key", "default") for default vals
         architecture = yamlfile["options"]["architecture"]
         task_name = yamlfile["task_name"]
-        dataset_dir = Path(cwd,yamlfile["dataset"]).resolve()
+        dataset_dir = Path(cwd, yamlfile["dataset"]).resolve()
         method = yamlfile["options"]["method"]
         is_kaggle = yamlfile["options"]["kaggle"]
-        
+
         approach = yamlfile["options"]["approach"]
         pooling = yamlfile["options"]["pooling"]
 
-        model_loc = run_training_task(architecture, task_name, dataset_dir, method, is_kaggle)
+        model_loc = run_training_task(
+            architecture, task_name, dataset_dir, method, is_kaggle)
 
-        shutil.copyfile(Path(cwd, file_loc).resolve(), Path(model_loc, "task_config.yml").resolve())
+        shutil.copyfile(Path(cwd, file_loc).resolve(), Path(
+            model_loc, "task_config.yml").resolve())
+
 
 def run_training_task(architecture, task_name, dataset_dir, method, is_kaggle, approach=None, pooling=None):
     trained_models_path = Path(
         filedir, "../../out/trained_models").resolve()
-    
+
     trained_models_path.mkdir(parents=True, exist_ok=True)
 
     print("Devices: ", list_logical_devices())
@@ -63,11 +70,19 @@ def run_training_task(architecture, task_name, dataset_dir, method, is_kaggle, a
     model = create_model(architecture, method, is_kaggle)
 
     start = datetime.now()
-    
+
+    lr_reducer = ReduceLROnPlateau(factor=0.1,
+                                   verbose=1,
+                                   cooldown=0,
+                                   patience=5,
+                                   min_lr=0.5e-6)
+
+    callbacks = [lr_reducer]
+
     history = model.fit(train_images,
-                        validation_data=val_images
-                        epochs=18, # add opt for this
-                        verbose=1)
+                        validation_data=val_images,
+                        epochs=18,  # add opt for this
+                        verbose=1, callbacks=callbacks)
 
     duration = datetime.now() - start
     print("Training completed in time: ", duration)
@@ -78,12 +93,12 @@ def run_training_task(architecture, task_name, dataset_dir, method, is_kaggle, a
 
     model_loc = Path(trained_models_path, task_name)
     model.save(model_loc)
-    
-    with open(Path(model_loc,"stats"), "w") as f:
+
+    with open(Path(model_loc, "stats"), "w") as f:
         f.write("Training completed in time: {}\n".format(duration))
         f.write("Test loss: {}".format(score[0]))
         f.write("Test accuracy: {}".format(score[1]))
 
     plot_data(history, Path(model_loc, '{}_plt.png'.format(task_name)))
-    
+
     return model_loc
