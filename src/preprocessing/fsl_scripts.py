5f1e3d4ec1cf0b37144c6dc38c51c4d2c63da41d
tpremoli <tomas@premoli.org>
2023-03-01 01:44:50 +0000

    slight tweaks to fsl launch

--- src/preprocessing/fsl_scripts.py
+++ src/preprocessing/fsl_scripts.py
@@ -38,27 +38,27 @@ def run_fsl(scan_location, scan_name, group, out_dir):
         raise ValueError(
             f"ERROR: FSL failed to run on scan {scan_name} in group {group}. \n Original brain: {scan_location} \n tmp_dir: {tmp_dir}")
 
+    cprint("INFO: fsl_anat complete. Running flirt to register to MNI space", "blue")
+    # We're runnning flirt with custom parameters to improve resolution from 2mm to 1mm
+    flirt(src=Path(anat_dir, "T1_biascorr"),
+            ref="$FSLDIR/data/standard/MNI152_T1_1mm",
+            interp="spline",
+            dof=12,
+            v=True,
+            omat=Path(anat_dir, "T1_to_MNI_lin.mat"),
+            out=Path(anat_dir, "T1_to_MNI_lin")
+            )
+
+    bet(Path(anat_dir, "T1_to_MNI_lin.nii.gz"), 
+        Path(anat_dir, "T1_to_MNI_lin_brain"), f=0.3, g=0, t=True)
+    
+    cprint("INFO: finalized flirt and brain extraction", "blue")
+    
     if USE_NONLINEAR_REGISTRATION:
         # TODO: Implement nonlinear registration
         raise NotImplementedError(
             "Nonlinear registration not yet implemented!"
         )
-    else:
-        cprint("fsl_anat complete. Running flirt to register to MNI space", "green")
-        # We're runnning flirt with custom parameters to improve resolution from 2mm to 1mm
-        flirt(src=Path(anat_dir, "T1_biascorr"),
-              ref="$FSLDIR/data/standard/MNI152_T1_1mm",
-              interp="spline",
-              dof=12,
-              v=True,
-              omat=Path(anat_dir, "T1_to_MNI_lin.mat"),
-              out=Path(anat_dir, "T1_to_MNI_lin")
-              )
-
-        bet(Path(anat_dir, "T1_to_MNI_lin.nii.gz"), 
-            Path(anat_dir, "T1_to_MNI_lin_brain"), f=0.3, g=0, t=True)
-        
-        cprint("fsl_anat complete. Running flirt to register to MNI space", "green")
 
     # File is saved into group subfolder in nii_files output loc
     final_brain = Path(
