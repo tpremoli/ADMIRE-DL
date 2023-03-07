f9555360d88ae54462d8c8f4427637dff029b541
tpremoli <tomas@premoli.org>
2023-03-07 18:10:06 +0000

    first stages of eval_model

--- src/evaluating/eval_model.py
+++ src/evaluating/eval_model.py
@@ -1,17 +1,43 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.applications.vgg16 import preprocess_input, decode_predictions
+import cv2
+from tensorflow.keras import applications as apps
 from keras.models import load_model
 from pathlib import Path
+from termcolor import cprint
+
+from .utils import load_config
+from ..constants import *
 
 cwd = Path().resolve()
 filedir = Path(__file__).parent.resolve()
 
-model = load_model('out/trained_models/adni/vgg16_pretrain_slices')
+def main():
 
-architecture = "VGG16"
-is_kaggle = False
-dataset_dir = "/home/tpremoli/MRI_AD_Diagnosis/out/preprocessed_datasets/adni_processed/slice_dataset"
+    model = load_model('out/trained_models/adni/tl_mc_resnet152_maxpooling_aug_adam_skipslices')
+    config = load_config('out/trained_models/adni/tl_mc_resnet152_maxpooling_aug_adam_skipslices')
+    
 
-model.summary()
+    cprint(f"INFO: Loaded task {config['task_name']}", "blue")
+
+    dataset = Path(cwd, config['dataset']).resolve()
+    test = Path(dataset, 'test').resolve()
+    val = Path(dataset, 'val').resolve()
+    
+    # getting the model's preprocessing function
+    model_preprocessing_func = apps.__dict__[
+        KERAS_APP_PKG[config["options"]["architecture"]]].preprocess_input
+    
+    for f in sorted(test.rglob('*.png')):
+        strpath = str(f)
+        # The local path to our target image
+        img = cv2.imread(strpath)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        X = np.expand_dims(img, axis=0).astype(np.float32)
+        X = model_preprocessing_func(X)
+        X = np.reshape(X, (1, 218, 182, 3))
+
+        # Remove last layer's softmax
+        preds = model.predict(X, verbose=0)
+        
+        print(f"predicted {f.name} as {preds[0][0]}, expected {f.parent.name}")
