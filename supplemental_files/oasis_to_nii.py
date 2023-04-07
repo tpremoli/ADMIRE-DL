5875a6934d39b93081513eb91c270eec35b43945
tpremoli <tomas@premoli.org>
2023-04-07 13:44:10 +0200

    significantly cleaned up oasis_to_nii.py

--- supplemental_files/oasis_to_nii.py
+++ supplemental_files/oasis_to_nii.py
@@ -1,51 +1,63 @@
 from pathlib import Path
+from termcolor import cprint
+import shutil
 import os
 import subprocess
+import sys
 
-# TODO: take args
-# Directory where the OASIS data is located
-rootdir = Path('/home/tpremoli/MRI_AD_Diagnosis/supplemental_files/unprocessed_datasets/OASIS')
+cwd = Path.cwd()
 
-# Directory to save results
-niftidir = rootdir.parent / 'oasis_nifti'
+if __name__ == '__main__':
+    # get rootdir from args
+    if len(sys.argv) != 1:
+        raise ValueError("ERROR: Need one argument - rootdir of OASIS dataset")
+    
+    rootdir = Path(cwd, sys.argv[1]).resolve()
+    
+    # Directory to save results
+    niftidir = rootdir.parent / 'oasis_nifti'
 
-# Create directory to save the results, if not existing
-(niftidir).mkdir(parents=True, exist_ok=True)
+    # Create directory to save the results, if not existing
+    (niftidir).mkdir(parents=True, exist_ok=True)
 
-# For each subject
-for s in sorted(rootdir.iterdir()):
-    # Skip non-directory entries
-    if not s.is_dir():
-        continue
-
-    # Some feedback in the screen
-    print(s.name)
-
-
-
-    # Directory of the original, raw data
-    raw_data_path = s / 'RAW'
-
-    # For each acquisition
-    for a in sorted(raw_data_path.iterdir()):
-        if not a.name.endswith('.hdr'):
+    # For each subject
+    for s in sorted(rootdir.iterdir()):
+        # Skip non-directory entries
+        if not s.is_dir():
             continue
 
-        output_file = niftidir / a.stem
+        # Some feedback in the screen
+        print(s.name)
 
-        # Do each of the 6 steps described in the blog
-        commands = [
-            f"{os.environ['FSLDIR']}/bin/fslchfiletype NIFTI_GZ {a} {output_file}",
-            f"{os.environ['FSLDIR']}/bin/fslorient -deleteorient {output_file}",
-            f"{os.environ['FSLDIR']}/bin/fslorient -setsformcode 2 {output_file}",
-            f"{os.environ['FSLDIR']}/bin/fslorient -setsform 0 0 -1.25 0 1 0 0 0 0 1 0 0 0 0 0 1 {output_file}",
-            f"{os.environ['FSLDIR']}/bin/fslswapdim {output_file} RL PA IS {output_file}",
-            f"{os.environ['FSLDIR']}/bin/fslorient -setqform -1.25 0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 {output_file}"
-        ]
+        # Directory of the original, raw data
+        raw_data_path = s / 'RAW'
 
-        for command in commands:
-            subprocess.run(command, shell=True, check=True)
-            
-        break # Only do the first acquisition
+        # For each acquisition
+        for a in sorted(raw_data_path.iterdir()):
+            if not a.name.endswith('.hdr'):
+                continue
 
-print("Done!")
+            output_file = niftidir / a.stem
+
+            # Do each of the 6 steps described in the blog
+            commands = [
+                f"{os.environ['FSLDIR']}/bin/fslchfiletype NIFTI_GZ {a} {output_file}",
+                f"{os.environ['FSLDIR']}/bin/fslorient -deleteorient {output_file}",
+                f"{os.environ['FSLDIR']}/bin/fslorient -setsformcode 2 {output_file}",
+                f"{os.environ['FSLDIR']}/bin/fslorient -setsform 0 0 -1.25 0 1 0 0 0 0 1 0 0 0 0 0 1 {output_file}",
+                f"{os.environ['FSLDIR']}/bin/fslswapdim {output_file} RL PA IS {output_file}",
+                f"{os.environ['FSLDIR']}/bin/fslorient -setqform -1.25 0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 {output_file}"
+            ]
+
+            for command in commands:
+                subprocess.run(command, shell=True, check=True)
+                
+            break # Only do the first acquisition
+    
+    # copy oasis.csv file to nifti directory
+    # this file contains the labels for the subjects
+    shutil.copyfile(rootdir.parent / 'OASIS.csv', niftidir / 'OASIS.csv')
+
+    cprint("SUCCESS: Successfully converted all oasis files to nii!", "green")
+    cprint("INFO: output files are in: " + str(niftidir), "yellow")
+    
