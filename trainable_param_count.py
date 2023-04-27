cb383997d99feb86338d33d450c10e855e161de8
tpremoli <tomas@premoli.org>
2023-04-27 16:59:37 +0100

    fixed up trainable_param_count.py

--- trainable_param_count.py
+++ trainable_param_count.py
@@ -1,21 +1,24 @@
 """This script counts the number of trainable parameters in each model
 in the out/trained_models directory and writes the results to
-out/trainable_param_counts.txt.
+out/trainable_param_counts.csv.
 """
 
 from pathlib import Path
 from keras.utils.layer_utils import count_params
 from keras.models import load_model
+from termcolor import cprint
 
 if __name__ == "__main__":
     print("Counting trainable parameters...")
     
-    with open("out/trainable_param_counts.txt", "w") as f:
-        f.write("Model: Trainable Parameters\n")
+    # open the file to write to as csv
+    with open("out/trainable_param_counts.csv", "w") as f:
+        f.write("model,trainable_params\n")
     
-        for path in Path("out/trained_models").glob("*"):
+        for path in sorted(Path("out/trained_models").glob("*")):
             if path.is_dir():
                 # Load every model
                 model = load_model(path)
                 trainable_count = count_params(model.trainable_weights)
-                f.write(f"{path.name}: {trainable_count}")
+                f.write(f"{path.name},{trainable_count}\n")
+                cprint("Counted trainable parameters for " + path.name, "green")
